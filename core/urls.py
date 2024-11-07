70f89b5e232394111b048142593e537f67624634
Salohiddin <bcloudintelekt@gmail.com>
2024-11-07 11:59:18 +0500

    configured static files

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,13 @@
-from django.contrib import admin
-from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from django.http import JsonResponse
+from django.urls import path
+from django.contrib import admin
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('health/', lambda _: JsonResponse({'detail': 'Healthy'}), name='health'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
